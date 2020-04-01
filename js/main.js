a0130f4298de67528bd72f712f6e42d52b066532
Wopnis <wopnis2016@outlook.com>
2020-04-01 13:18:03 +0300

    add socpal animation

--- js/main.js
+++ js/main.js
@@ -17,4 +17,42 @@ for (let anchor of anchors) {
 }
 
 // modal ===========
+{
+    const modal = document.getElementById('contactModal'),
+          btn = document.getElementById('contactBtn')
+          cross = document.querySelector('.close')
+          contactsSocialBtn = document.querySelector('.nav_btn'),
+          contactsSocialBlock = document.querySelector('.social'),
+          list = document.querySelector('.list');
+
+//  console.log("clicked");   
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.style.display = 'block';
+});
+cross.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if(event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+contactsSocialBtn.addEventListener('click', (event) => {
+    contactsSocialBlock.style.cssText = `
+        position: absolute;
+        left: 700px;
+        top: 2400px;
+        transform: scale(2.5);
+        text-decoraition: outline;
+    `;
+});
+
+
+
+}
 
